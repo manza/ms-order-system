128e1102225ef61950b2444ce88feed4504c95ed
manza <reinaldo.manzano@gmail.com>
2018-05-11 08:03:06 -0300

    first commit

---
+++ src/main/java/com/manzano/msordersystem/MsOrderSystemApplication.java
@@ -0,0 +1,12 @@
+package com.manzano.msordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOrderSystemApplication.class, args);
+	}
+}
